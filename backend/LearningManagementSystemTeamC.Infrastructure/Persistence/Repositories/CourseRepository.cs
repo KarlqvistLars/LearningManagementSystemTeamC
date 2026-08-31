34a3fe038c03c233cde2c01d63c42230cd23ab9a
Johanna Sewring <j_sewring@hotmail.com>
2026-08-31 17:29:40 +0200

    feature view courses: add repository methods for fetching courses

--- backend/LearningManagementSystemTeamC.Infrastructure/Persistence/Repositories/CourseRepository.cs
+++ backend/LearningManagementSystemTeamC.Infrastructure/Persistence/Repositories/CourseRepository.cs
@@ -1,5 +1,8 @@
+using LearningManagementSystemTeamC.Application.Common.DTOs;
 using LearningManagementSystemTeamC.Application.Courses;
 using LearningManagementSystemTeamC.Domain.Courses;
+using Microsoft.EntityFrameworkCore;
+using LearningManagementSystemTeamC.Application.Common.Mappers;
 
 namespace LearningManagementSystemTeamC.Infrastructure.Persistence.Repositories;
 
@@ -10,6 +13,21 @@ public class CourseRepository : ICourseRepository
     public CourseRepository(ApplicationDbContext context)
     {
         _context = context;
+    }
+
+    public async Task<IEnumerable<CourseDto>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        return await _context.Courses
+            .Select(CourseMapper.CourseToDto)
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task<CourseDto?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        return await _context.Courses
+            .Where(course => course.Id == id)
+            .Select(CourseMapper.CourseToDto)
+            .SingleOrDefaultAsync(cancellationToken);
     }
 
     public async Task AddAsync(
