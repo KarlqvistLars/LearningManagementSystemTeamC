0db25f295729d1eb4368289dc847aaa02a0d59f2
Johanna Sewring <j_sewring@hotmail.com>
2026-08-31 17:22:51 +0200

    feature view courses: add mapper expression from entity to dto

---
+++ backend/LearningManagementSystemTeamC.Application/Common/Mappers/CourseMapper.cs
@@ -0,0 +1,16 @@
+﻿using LearningManagementSystemTeamC.Application.Common.DTOs;
+using LearningManagementSystemTeamC.Domain.Courses;
+using System.Linq.Expressions;
+
+namespace LearningManagementSystemTeamC.Application.Common.Mappers;
+
+public static class CourseMapper
+{
+    public static readonly Expression<Func<Course, CourseDto>> CourseToDto =
+    course => new CourseDto(
+        course.Id,
+        course.CourseName,
+        course.Description,
+        course.StartDate,
+        course.EndDate);
+}
