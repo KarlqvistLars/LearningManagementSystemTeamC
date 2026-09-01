4a5325e265929632b6d514235513cda58c33e3d6
Nordic-Panda <odielee@hotmail.com>
2026-09-01 08:16:55 +0200

    feat: added user Repo and configuration

---
+++ backend/LearningManagementSystemTeamC.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using LearningManagementSystemTeamC.Domain.Roles;
+using LearningManagementSystemTeamC.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LearningManagementSystemTeamC.Infrastructure.Persistence.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.RoleId).IsRequired();
+        builder.HasOne<Role>()
+            .WithMany()
+            .HasForeignKey(x => x.RoleId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Property(x => x.Email)
+            .IsRequired()
+            .HasMaxLength(UserRules.EmailMaxLength);
+        builder.HasIndex(x => x.Email)
+            .IsUnique();
+
+        builder.Property(x => x.CreatedAt)
+            .IsRequired();
+    }
+}
---
+++ backend/LearningManagementSystemTeamC.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using LearningManagementSystemTeamC.Application.Users;
+using LearningManagementSystemTeamC.Domain.Users;
+
+namespace LearningManagementSystemTeamC.Infrastructure.Persistence.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public UserRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(User user, CancellationToken cancellationToken)
+    {
+        await _context.Users.AddAsync(user, cancellationToken);
+    }
+}
