fc4dd5e5a30ed9c7fb89ad40fa81224f8d8abc83
Nordic-Panda <odielee@hotmail.com>
2026-09-01 08:06:09 +0200

    feat: added user entity

---
+++ backend/LearningManagementSystemTeamC.Domain/Users/User.cs
@@ -0,0 +1,33 @@
+﻿using LearningManagementSystemTeamC.Domain.Common.Exceptions;
+
+namespace LearningManagementSystemTeamC.Domain.Users;
+
+public class User
+{
+    public Guid Id { get; private set; }
+    public string Email { get; private set; } = string.Empty;
+    public Guid RoleId { get; private set; }
+    public bool IsActive { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+
+    public void Disable() => IsActive = false;
+
+    public void Enable() => IsActive = true;
+
+    private User() { }
+
+    public  User(string email, Guid roleId)
+    {
+        if (string.IsNullOrWhiteSpace(email))
+            throw new DomainException(UserRules.EmailRequiredCode, UserRules.EmailRequiredMessage);
+
+        if (roleId == Guid.Empty)
+            throw new DomainException(UserRules.RoleRequiredCode, UserRules.RoleRequiredMessage);
+
+        Id = Guid.NewGuid();
+        Email = email;
+        RoleId = roleId;
+        IsActive = true;
+        CreatedAt = DateTime.UtcNow;
+    }
+}
---
+++ backend/LearningManagementSystemTeamC.Domain/Users/UserRules.cs
@@ -0,0 +1,10 @@
+﻿namespace LearningManagementSystemTeamC.Domain.Users;
+
+public class UserRules
+{
+    public const string EmailRequiredCode = "USER_EMAIL_REQUIRED";
+    public const string EmailRequiredMessage = "Email is required.";
+
+    public const string RoleRequiredCode = "USER_ROLE_REQUIRED";
+    public const string RoleRequiredMessage = "Role is required.";
+}
