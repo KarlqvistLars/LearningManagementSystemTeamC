04487bc808df92f989c0404b4f01a03d31a0926e
Nordic-Panda <odielee@hotmail.com>
2026-09-01 08:52:09 +0200

    feat: added role repository

---
+++ backend/LearningManagementSystemTeamC.Application/Roles/IRoleRepository.cs
@@ -0,0 +1,8 @@
+﻿using LearningManagementSystemTeamC.Domain.Roles;
+
+namespace LearningManagementSystemTeamC.Application.Roles;
+
+public interface IRoleRepository
+{
+    Task<Role?> GetActiveById(Guid id);
+}
