888f5956b0de1115945795f52b2844aff47e4b5d
kostas <konstantinidis.kon@yahoo.com>
2026-09-03 16:22:32 +0300

    add frontend for viewing activities by module

--- frontend/lms-frontend-g3/src/app/routes.tsx
+++ frontend/lms-frontend-g3/src/app/routes.tsx
@@ -8,6 +8,7 @@ import { MainPage } from '../features/MainPage';
 import { CoursePage } from '../features/courses/CoursePage';
 import { NotFoundPage } from '../features/not-found/NotFoundPage';
 import { LoginPage } from '../features/login/LoginPage';
+import { ModuleActivitiesPage } from '../features/activities/pages/ModuleActivitiesPage';
 
 export function AppRoutes() {
     return (
@@ -15,6 +16,7 @@ export function AppRoutes() {
             <Route path="/" element={<MainLayout />}>
                 <Route index element={<MainPage />} />
                 <Route path="/courses" element={<CoursePage />} />
+                <Route path="/modules/:moduleId/activities" element={<ModuleActivitiesPage />} />
                 <Route path="/login" element={<LoginPage />} />    
             </Route>
             <Route path="*" element={<NotFoundPage />} />
