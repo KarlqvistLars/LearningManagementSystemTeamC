463a28a5427b0dcbd1fa58ffdc4f5db21978d6bd
kostas <konstantinidis.kon@yahoo.com>
2026-09-03 13:45:11 +0300

    add GetActivitiesByModuleHandler.cs

---
+++ backend/LearningManagementSystemTeamC.Application/Activities/Queries/GetActivitiesByModule/GetActivitiesByModuleHandler.cs
@@ -0,0 +1,35 @@
+using LearningManagementSystemTeamC.Application.Common.DTOs;
+using LearningManagementSystemTeamC.Application.Common.Interfaces;
+
+namespace LearningManagementSystemTeamC.Application.Activities.Queries.GetActivitiesByModule;
+
+public class GetActivitiesByModuleHandler
+{
+    private readonly IActivityRepository _activityRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public GetActivitiesByModuleHandler(
+        IActivityRepository activityRepository,
+        IUnitOfWork unitOfWork)
+    {
+        _activityRepository = activityRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<IReadOnlyList<ActivityDto>> Handle(
+        GetActivitiesByModuleQuery query)
+    {
+        var activities = await _activityRepository.GetActivitiesByModuleIdAsync(query.ModuleId);
+
+        return activities.Select(activity => new ActivityDto(
+            activity.Id,
+            activity.ActivityName,
+            activity.Description,
+            activity.StartDate,
+            activity.EndDate,
+            activity.Type,
+            activity.ModuleId,
+            activity.Module.ModuleName))
+            .ToList();
+    }
+}
