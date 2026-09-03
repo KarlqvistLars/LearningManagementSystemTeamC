13d161eef782d23f1371a9c78aae4589aaf9423b
kostas <konstantinidis.kon@yahoo.com>
2026-09-03 13:45:11 +0300

    add GetActivitiesByModuleHandler.cs

--- backend/LearningManagementSystemTeamC.Application/Activities/Queries/GetActivitiesByModule/GetActivitiesByModuleQuery.cs
+++ backend/LearningManagementSystemTeamC.Application/Activities/Queries/GetActivitiesByModule/GetActivitiesByModuleQuery.cs
@@ -1,3 +1,3 @@
-namespace LearningManagementSystemTeamC.Application.Activities.Queries.GetActivityByModule;
+namespace LearningManagementSystemTeamC.Application.Activities.Queries.GetActivitiesByModule;
 
 public record GetActivitiesByModuleQuery(Guid ModuleId);
