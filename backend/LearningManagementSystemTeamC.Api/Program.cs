6dd4f5a7d7acfcd295df1b6c23c45e1c49da7e06
Nordic-Panda <odielee@hotmail.com>
2026-09-03 19:19:03 +0200

    feat: activated swagger auth

--- backend/LearningManagementSystemTeamC.Api/Program.cs
+++ backend/LearningManagementSystemTeamC.Api/Program.cs
@@ -5,9 +5,7 @@ using LearningManagementSystemTeamC.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.ActiveSwaggerAuthentication();
 
 builder.Services
     .AddApplication()
