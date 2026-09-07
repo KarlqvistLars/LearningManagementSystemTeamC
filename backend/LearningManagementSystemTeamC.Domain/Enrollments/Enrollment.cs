a501d444b9d6747a7a0887d9e5a2e5448410e771
Johanna Sewring <j_sewring@hotmail.com>
2026-09-07 15:36:35 +0200

    feature: add enrollment entity

---
+++ backend/LearningManagementSystemTeamC.Domain/Enrollments/Enrollment.cs
@@ -0,0 +1,17 @@
+﻿namespace LearningManagementSystemTeamC.Domain.Enrollments;
+
+public class Enrollment
+{
+    public Guid Id { get; private set; }
+    public Guid UserId { get; private set; }
+    public Guid CourseId { get; private set; }
+    public DateTime EnrolledAt { get; private set; }
+    public bool IsActive { get; private set; } = false;
+    public Enrollment(Guid userId, Guid courseId)
+    {
+        Id = Guid.NewGuid();
+        UserId = userId;
+        CourseId = courseId;
+        EnrolledAt = DateTime.UtcNow;
+    }
+}
