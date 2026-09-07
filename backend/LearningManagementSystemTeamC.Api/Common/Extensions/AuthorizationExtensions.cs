675d51e0dcea4efbfab4d48780aa168caa1b7a14
Nordic-Panda <odielee@hotmail.com>
2026-09-07 09:57:19 +0200

    feat: added policies

---
+++ backend/LearningManagementSystemTeamC.Api/Common/Extensions/AuthorizationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using LearningManagementSystemTeamC.Api.Common.Constants;
+using LearningManagementSystemTeamC.Domain.Roles;
+
+namespace LearningManagementSystemTeamC.Api.Common.Extensions;
+
+public static class AuthorizationExtensions
+{
+    public static IServiceCollection AddAuthorizationPolicies(this IServiceCollection services)
+    {
+        services
+            .AddAuthorizationBuilder()
+            .AddPolicy(PolicyConstants.AuthenticatedUser, policy => policy.RequireAuthenticatedUser())
+            .AddPolicy(
+                PolicyConstants.TeacherOnly,
+                policy => policy.RequireRole(RoleRules.TeacherRoleCode)
+            );
+
+        return services;
+    }
+}
