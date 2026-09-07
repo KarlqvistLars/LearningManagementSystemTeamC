0a70901890d91fe15aadb3188e3432bda660bc0e
Johanna Sewring <j_sewring@hotmail.com>
2026-09-07 15:36:35 +0200

    feature: add enrollment entity

--- backend/LearningManagementSystemTeamC.Infrastructure/Persistence/ApplicationDbContext.cs
+++ backend/LearningManagementSystemTeamC.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using LearningManagementSystemTeamC.Domain.Roles;
 using LearningManagementSystemTeamC.Domain.Users;
 using LearningManagementSystemTeamC.Domain.Modules;
 using Microsoft.EntityFrameworkCore;
+using LearningManagementSystemTeamC.Domain.Enrollments;
 
 namespace LearningManagementSystemTeamC.Infrastructure.Persistence;
 
@@ -18,6 +19,7 @@ public class ApplicationDbContext : DbContext
     public DbSet<User> Users => Set<User>();
     public DbSet<Role> Roles => Set<Role>();
     public DbSet<Module> Modules => Set<Module>();
+    public DbSet<Enrollment> Enrollments => Set<Enrollment>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
