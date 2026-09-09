6a71ffacace1cfba104b4e4ccedd0ce800e54095
Nordic-Panda <odielee@hotmail.com>
2026-09-09 05:10:48 +0200

    feat: added userinfo entity, rules. refactored user entity

--- backend/LearningManagementSystemTeamC.Domain/UserInfos/UserInfo.cs
+++ backend/LearningManagementSystemTeamC.Domain/UserInfos/UserInfo.cs
@@ -1,13 +1,76 @@
-﻿namespace LearningManagementSystemTeamC.Domain.UserInfos;
+﻿using LearningManagementSystemTeamC.Domain.Common.Exceptions;
+
+namespace LearningManagementSystemTeamC.Domain.UserInfos;
 
 public class UserInfo
 {
+    public Guid Id { get; private set; }
     public Guid UserId { get; private set; }
     public string FirstName { get; private set; } = string.Empty;
-    public string LastName { get; private set;} = string.Empty;
+    public string LastName { get; private set; } = string.Empty;
     public DateTime DateOfBirth { get; private set; }
     public string PhoneNumber { get; private set; } = string.Empty;
     public string Address { get; private set; } = string.Empty;
     public string PostalCode { get; private set; } = string.Empty;
     public string City { get; private set; } = string.Empty;
+
+    private UserInfo() { }
+
+    public UserInfo(Guid userId, string fName, string lName, DateTime dateOfBirth, string phoneNumber, string address, string postalCode, string city)
+    {
+        Validate(
+            userId,
+            fName,
+            lName,
+            dateOfBirth,
+            phoneNumber,
+            address,
+            postalCode,
+            city);
+
+        Id = Guid.NewGuid();
+        UserId = userId;
+        FirstName = fName;
+        LastName = lName;
+        DateOfBirth = dateOfBirth;
+        PhoneNumber = phoneNumber;
+        Address = address;
+        PostalCode = postalCode;
+        City = city;
+    }
+
+    private static void Validate(
+        Guid userId,
+        string fName,
+        string lName,
+        DateTime dateOfBirth,
+        string phoneNumber,
+        string address,
+        string postalCode,
+        string city)
+    {
+        if (userId == Guid.Empty)
+            throw new DomainException(UserInfoRules.UserIdRequiredCode, UserInfoRules.UserIdRequiredMessage);
+
+        if (string.IsNullOrWhiteSpace(fName))
+            throw new DomainException(UserInfoRules.FirstNameRequiredCode, UserInfoRules.FirstNameRequiredMessage);
+
+        if (string.IsNullOrWhiteSpace(lName))
+            throw new DomainException(UserInfoRules.LastNameRequiredCode, UserInfoRules.LastNameRequiredMessage);
+
+        if (dateOfBirth == DateTime.MinValue)
+            throw new DomainException(UserInfoRules.DateOfBirthRequiredCode, UserInfoRules.DateOfBirthRequiredMessage);
+
+        if (string.IsNullOrWhiteSpace(phoneNumber))
+            throw new DomainException(UserInfoRules.PhoneNumberRequiredCode, UserInfoRules.PhoneNumberRequiredMessage);
+
+        if (string.IsNullOrWhiteSpace(address))
+            throw new DomainException(UserInfoRules.AddressRequiredCode, UserInfoRules.AddressRequiredMessage);
+
+        if (string.IsNullOrWhiteSpace(postalCode))
+            throw new DomainException(UserInfoRules.PostalCodeRequiredCode, UserInfoRules.PostalCodeRequiredMessage);
+
+        if (string.IsNullOrWhiteSpace(city))
+            throw new DomainException(UserInfoRules.CityRequiredCode, UserInfoRules.CityRequiredMessage);
+    }
 }
---
+++ backend/LearningManagementSystemTeamC.Domain/UserInfos/UserInfoRules.cs
@@ -0,0 +1,21 @@
+﻿namespace LearningManagementSystemTeamC.Domain.UserInfos;
+
+public class UserInfoRules
+{
+    public const string UserIdRequiredCode = "USERINFO_USERID_REQUIRED";
+    public const string UserIdRequiredMessage = "User ID is required.";
+    public const string FirstNameRequiredCode = "USERINFO_FIRSTNAME_REQUIRED";
+    public const string FirstNameRequiredMessage = "First name is required.";
+    public const string LastNameRequiredCode = "USERINFO_LASTNAME_REQUIRED";
+    public const string LastNameRequiredMessage = "Last name is required.";
+    public const string DateOfBirthRequiredCode = "USERINFO_DATEOFBIRTH_REQUIRED";
+    public const string DateOfBirthRequiredMessage = "Date of birth is required.";
+    public const string PhoneNumberRequiredCode = "USERINFO_PHONENUMBER_REQUIRED";
+    public const string PhoneNumberRequiredMessage = "Phone number is required.";
+    public const string AddressRequiredCode = "USERINFO_ADDRESS_REQUIRED";
+    public const string AddressRequiredMessage = "Address is required.";
+    public const string PostalCodeRequiredCode = "USERINFO_POSTALCODE_REQUIRED";
+    public const string PostalCodeRequiredMessage = "Postal code is required.";
+    public const string CityRequiredCode = "USERINFO_CITY_REQUIRED";
+    public const string CityRequiredMessage = "City is required.";
+}
