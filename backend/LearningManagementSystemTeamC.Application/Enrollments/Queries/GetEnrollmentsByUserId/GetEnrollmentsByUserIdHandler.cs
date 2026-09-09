056b70ab1c1af8002570065f5de53fb10c8e29a9
Johanna Sewring <j_sewring@hotmail.com>
2026-09-09 11:35:12 +0200

    refactor: check if user exist and is active

--- backend/LearningManagementSystemTeamC.Application/Enrollments/Queries/GetEnrollmentsByUserId/GetEnrollmentsByUserIdHandler.cs
+++ backend/LearningManagementSystemTeamC.Application/Enrollments/Queries/GetEnrollmentsByUserId/GetEnrollmentsByUserIdHandler.cs
@@ -1,6 +1,9 @@
 ﻿using LearningManagementSystemTeamC.Application.Common.DTOs;
 using LearningManagementSystemTeamC.Application.Common.Mappers;
 using LearningManagementSystemTeamC.Application.Courses;
+using LearningManagementSystemTeamC.Application.Users;
+using LearningManagementSystemTeamC.Domain.Common.Exceptions;
+using LearningManagementSystemTeamC.Domain.Users;
 
 namespace LearningManagementSystemTeamC.Application.Enrollments.Queries.GetEnrollmentsByUserId;
 
@@ -8,17 +11,34 @@ public class GetEnrollmentsByUserIdHandler : IGetEnrollmentsByUserIdHandler
 {
     private readonly IEnrollmentRepository _enrollmentRepository;
     private readonly ICourseRepository _courseRepository;
+    private readonly IUserRepository _userRepository;
 
     public GetEnrollmentsByUserIdHandler(
         IEnrollmentRepository enrollmentRepository,
-        ICourseRepository courseRepository)
+        ICourseRepository courseRepository,
+        IUserRepository userRepository)
     {
         _enrollmentRepository = enrollmentRepository;
         _courseRepository = courseRepository;
+        _userRepository = userRepository;
     }
 
     public async Task<IEnumerable<CourseDto>> Handle(GetEnrollmentsByUserIdQuery query, CancellationToken cancellationToken)
     {
+        var user = await _userRepository.GetByIdAsync(query.UserId, cancellationToken);
+        if (user == null)
+        {
+            throw new NotFoundException(
+                UserRules.UserNotFoundCode,
+                UserRules.UserNotFoundMessage);
+        }
+        else if (!user.IsActive)
+        {
+            throw new NotFoundException(
+                UserRules.AccountNotAvailableCode,
+                UserRules.AccountNotAvailableMessage);
+        }
+
         var result = await _enrollmentRepository.GetByUserIdAsync(query.UserId, cancellationToken);
         var enrolledCourses = result.Select(e => e.CourseId).ToList();
         var courses = await _courseRepository.GetCoursesByIdsAsync(enrolledCourses, cancellationToken);
