7505bb83184ac8f977c21d3192201ecd3d16095c
Nordic-Panda <odielee@hotmail.com>
2026-09-09 06:48:03 +0200

    feat: add updateuser endpoint

--- backend/LearningManagementSystemTeamC.Api/Controllers/UsersController.cs
+++ backend/LearningManagementSystemTeamC.Api/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using LearningManagementSystemTeamC.Api.Common.Contracts;
 using LearningManagementSystemTeamC.Application.Common.DTOs;
 using LearningManagementSystemTeamC.Application.Common.Interfaces;
 using LearningManagementSystemTeamC.Application.Users.Commands.CreateUser;
+using LearningManagementSystemTeamC.Application.Users.Commands.UpdateUser;
 using LearningManagementSystemTeamC.Application.Users.Queries.GetUserById;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -39,6 +40,35 @@ public class UsersController : ControllerBase
     public async Task<IActionResult> GetById([FromRoute] Guid id, [FromServices] IGetUserByIdHandler getUserByIdHandler, CancellationToken cancellationToken)
     {
         var userDto = await getUserByIdHandler.Handle(new GetUserByIdQuery(id), cancellationToken);
+        return Ok(ApiResponse<UserDto>.Ok(userDto));
+    }
+
+    [Authorize(Policy = PolicyConstants.AuthenticatedUser)]
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> Update(
+        [FromRoute] Guid id,
+        UpdateUserCommand command,
+        [FromServices] IUpdateUserHandler updateUserHandler,
+        [FromServices] IValidator<UpdateUserCommand> updateUserValidator,
+        CancellationToken cancellationToken)
+    {
+        var commandWithId = command with
+        {
+            UserId = id
+        };
+
+        var details = updateUserValidator.Validate(commandWithId);
+
+        if (details.Count > 0)
+            return BadRequest(ApiResponse<Dictionary<string, string[]>>.Fail(
+                ExceptionConstants.ValidationFailedCode,
+                ExceptionConstants.ValidationFailedMessage,
+                details));
+
+        var userDto = await updateUserHandler.Handle(
+            commandWithId,
+            cancellationToken);
+
         return Ok(ApiResponse<UserDto>.Ok(userDto));
     }
 }
