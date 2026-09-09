4c4265895c248e598f8d2c067fd5e6dd8643a419
Johanna Sewring <j_sewring@hotmail.com>
2026-09-09 12:36:16 +0200

    refactor: delete method synchronous

--- backend/LearningManagementSystemTeamC.Application/Enrollments/IEnrollmentRepository.cs
+++ backend/LearningManagementSystemTeamC.Application/Enrollments/IEnrollmentRepository.cs
@@ -5,7 +5,7 @@ namespace LearningManagementSystemTeamC.Application.Enrollments;
 public interface IEnrollmentRepository
 {
     Task AddAsync(Enrollment enrollment, CancellationToken cancellationToken);
-    Task DeleteAsync(Enrollment enrollment,  CancellationToken cancellationToken);
+    void DeleteAsync(Enrollment enrollment,  CancellationToken cancellationToken);
     Task<IEnumerable<Enrollment>> GetByUserIdAsync(Guid userId, CancellationToken cancellationToken);
     Task<IEnumerable<Enrollment>> GetByCourseIdAsync(Guid courseId, CancellationToken cancellationToken);
 }
