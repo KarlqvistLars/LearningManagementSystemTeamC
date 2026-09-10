d3942b7252b78caeb1193b9563498067ce5adf58
Nordic-Panda <odielee@hotmail.com>
2026-09-10 15:48:59 +0200

    refactor: move sidemenu to layout

--- frontend/lms-frontend-g3/src/features/MainPage.tsx
+++ frontend/lms-frontend-g3/src/features/MainPage.tsx
@@ -1,18 +1,3 @@
-import { useAuth } from "./auth/AuthContext";
-import { SideMenu } from "../shared/components/SideMenu";
-import {
-  studentMenuItems,
-  teacherMenuItems,
-} from "../shared/components/types/menu";
-
 export function MainPage() {
-  const { isTeacher } = useAuth();
-
-  const menuItems = isTeacher ? teacherMenuItems : studentMenuItems;
-
-  return (
-    <section className="flex">
-      <SideMenu menuItems={menuItems} />
-    </section>
-  );
+  return <section className="flex"></section>;
 }
