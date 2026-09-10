9e6b75dbd35391fc50de4939e1912e1ca0cc571a
Anders Hansen-Haug <andershh_93@hotmail.com>
2026-09-10 13:53:21 +0200

    Removed old migration files

--- frontend/lms-frontend-g3/src/features/modules/pages/ModulePage.tsx
+++ frontend/lms-frontend-g3/src/features/modules/pages/ModulePage.tsx
@@ -35,7 +35,9 @@ export function ModulePage() {
               <div>
                 {isTeacher && (
                   <button 
-                    onClick={() => setShowModuleForm(true)}
+                    onClick={() => {
+                      setSelectedModule(null);
+                      setShowModuleForm(true);}}
                     className="mt-16 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 hover:cursor-pointer">
                     Create new module
                   </button>
