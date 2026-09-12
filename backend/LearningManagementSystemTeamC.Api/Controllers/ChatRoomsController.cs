a26aedd71b6f811fd68c71d913558d4b21ea0cf1
Nordic-Panda <odielee@hotmail.com>
2026-09-12 15:02:15 +0200

    feat: add create endpoint

---
+++ backend/LearningManagementSystemTeamC.Api/Controllers/ChatRoomsController.cs
@@ -0,0 +1,35 @@
+﻿using LearningManagementSystemTeamC.Api.Common.Contracts;
+using LearningManagementSystemTeamC.Api.Common.Extensions;
+using LearningManagementSystemTeamC.Application.ChatRooms.Commands.CreateChatRoom;
+using LearningManagementSystemTeamC.Application.Common.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningManagementSystemTeamC.Api.Controllers;
+
+[ApiController]
+[Route("api/chatrooms")]
+[Authorize(Policy = "AuthenticatedUser")]
+public class ChatRoomsController : ControllerBase
+{
+
+    public ChatRoomsController()
+    {
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(
+        CreateChatRoomCommand command,
+        [FromServices] ICreateChatRoomHandler createChatRoomHandler,
+        CancellationToken cancellationToken)
+    {
+        var creatorId = User.GetUserId();
+
+        var chatRoomDto = await createChatRoomHandler.HandleAsync(
+            command,
+            creatorId,
+            cancellationToken);
+        // TODO: to createdataction
+        return Ok(ApiResponse<ChatRoomDto>.Ok(chatRoomDto));
+    }
+}
