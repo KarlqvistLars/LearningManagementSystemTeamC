9b8c8f791dd6db806cddd12c679ab10f9aab8197
Nordic-Panda <odielee@hotmail.com>
2026-09-13 15:35:59 +0200

    refactor: signalR check if user is member of the chatroom

--- backend/LearningManagementSystemTeamC.Api/Hubs/ChatHub.cs
+++ backend/LearningManagementSystemTeamC.Api/Hubs/ChatHub.cs
@@ -1,4 +1,8 @@
 ﻿using LearningManagementSystemTeamC.Api.Common.Constants;
+using LearningManagementSystemTeamC.Api.Common.Extensions;
+using LearningManagementSystemTeamC.Application.ChatRooms;
+using LearningManagementSystemTeamC.Domain.ChatRooms;
+using LearningManagementSystemTeamC.Domain.Common.Exceptions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
@@ -7,8 +11,36 @@ namespace LearningManagementSystemTeamC.Api.Hubs;
 [Authorize(Policy = PolicyConstants.AuthenticatedUser)]
 public class ChatHub : Hub
 {
+    private readonly IChatRoomRepository _chatRoomRepository;
+
+    public ChatHub(IChatRoomRepository chatRoomRepository)
+    {
+        _chatRoomRepository = chatRoomRepository;
+    }
+
     public async Task JoinRoom(Guid chatRoomId)
     {
+        var user = Context.User
+            ?? throw new UnauthorizedException(
+            ExceptionConstants.UnauthorizedCode,
+            ExceptionConstants.UnauthorizedMessage);
+
+        var userId = user.GetUserId();
+
+        var chatRoom = await _chatRoomRepository.GetByIdAsync(
+            chatRoomId,
+            Context.ConnectionAborted) ?? throw new NotFoundException(
+                ChatRoomRules.NotFoundCode,
+                ChatRoomRules.NotFoundMessage);
+
+        var isMember = chatRoom.Members
+            .Any(member => member.UserId == userId);
+
+        if (!isMember)
+            throw new UnauthorizedException(
+                ChatRoomRules.NotMemberCode,
+                ChatRoomRules.NotMemberMessage);
+
         await Groups.AddToGroupAsync(
             Context.ConnectionId,
             chatRoomId.ToString());
