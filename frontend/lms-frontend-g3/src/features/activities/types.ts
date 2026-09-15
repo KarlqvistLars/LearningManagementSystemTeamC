7f987016e1a97ce8ba62e7269c385c9944e06d61
cycloholic <konstantinidis.kon@yahoo.com>
2026-09-15 17:24:27 +0200

    Add CreateActivity and EditActivity types

--- frontend/lms-frontend-g3/src/features/activities/types.ts
+++ frontend/lms-frontend-g3/src/features/activities/types.ts
@@ -7,3 +7,22 @@ export interface ActivityDto {
     endDate: string;
     moduleId: string;
 }
+
+export interface CreateActivity {
+    activityName: string;
+    description: string;
+    startDate: string;
+    endDate: string;
+    type: string;
+    moduleId: string;
+}
+
+export interface EditActivity {
+    id: string;
+    activityName: string;
+    description: string;
+    startDate: string;
+    endDate: string;
+    type: string;
+    moduleId: string;
+}
